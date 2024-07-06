8255787bf30301a3c75d9a845bf72118e2490641
berk <abdulberk@gmail.com>
2024-07-06 21:58:50 +0300

    optimistic ui fix

--- todo-app/app/_hooks/useUpdateTodoMutation.ts
+++ todo-app/app/_hooks/useUpdateTodoMutation.ts
@@ -11,7 +11,7 @@ const updateTodo = async ({ id, updatedTodo }: { id: number | string; updatedTod
   const data: UpdatedTodoResponse = response.data;
 
   if (!data) {
-    throw new Error("Error updating todo!");
+      throw new Error("Error updating todo!");
   }
   return data;
 };
@@ -20,27 +20,32 @@ export const useUpdateTodoMutation = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationFn: (data: { id: number | string; updatedTodo: UpdateTodo }) => updateTodo(data),
-    onMutate: async ({ id, updatedTodo }) => {
-      await queryClient.cancelQueries({ queryKey: ["todos"] });
+      mutationFn: (data: { id: number | string; updatedTodo: UpdateTodo }) => updateTodo(data),
+      onMutate: async (updatedData: { id: number | string; updatedTodo: UpdateTodo }) => {
+          await queryClient.cancelQueries({ queryKey: ["todos"] });
 
-      const previousTodos = queryClient.getQueryData(["todos"]);
+          const previousTodos = queryClient.getQueryData<Todo[]>(["todos"]);
 
-      queryClient.setQueryData(["todos"], (oldTodos: any) => 
-        oldTodos.map((todo: Todo) => 
-          todo.id === id ? { ...todo, ...updatedTodo } : todo
-        )
-      );
+          queryClient.setQueryData<Todo[]>(["todos"], (oldTodos) =>
+              oldTodos?.map((todo) =>
+                  todo.id === updatedData.id ? { ...todo, ...updatedData.updatedTodo } : todo
+              )
+          );
 
-      return { previousTodos };
-    },
-    onError: (error, variables, context) => {
-      queryClient.setQueryData(["todos"], context?.previousTodos);
- 
-    },
-    onSuccess: (data, variables) => {
-      queryClient.invalidateQueries({ queryKey: ["todos"] });
-    }
+          return { previousTodos, updatedData };
+      },
+      onError: (error, variables, context) => {
+          if (context?.previousTodos) {
+              queryClient.setQueryData(["todos"], context.previousTodos);
+          }
+          
+      },
+      onSuccess: () => {
+          queryClient.invalidateQueries({ queryKey: ["todos"] });
+      },
+      onSettled: () => {
+          queryClient.invalidateQueries({ queryKey: ["todos"] });
+      },
   });
 };
   
